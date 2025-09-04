835b83367fd3565d92926d1c76b61e9cd75670d5
Joshua Borsatto <81537083+ferkepp@users.noreply.github.com>
2025-09-03 20:52:31 -0400

    Add files via upload

---
+++ JoshuaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshuaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoshuaDemoApplication.class, args);
+	}
+
+}
